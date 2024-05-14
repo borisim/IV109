4ebfa2758711b62d19a32d187a5607a1b4865ae8
Simon <519192@mail.muni.cz>
2024-05-15 00:01:34 +0200

    m

--- prod.py
+++ prod.py
@@ -45,7 +45,10 @@ year = 2024
 started = False
 paused = False
 population = x_male.sum() + x_female.sum()
-text = plt.figtext(.6,.9,"Population: {:,}, Year: {}".format(int(population), year), ha='center')
+dep = ((sum(x_male[0:7]) + sum(x_female[0:7]) + sum(x_male[32:]) + sum(x_female[32:])) 
+       / (sum(x_male[7:32]) + sum(x_female[7:32]))) * 100
+text = plt.figtext(.6,.9,"Population: {:,}, Year: {}, Dependency: {}".format(int(population), year, int(dep)), ha='center')
+
 
 
 
@@ -78,14 +81,45 @@ def reset(val):
     global population
     global year
     global text
+    global dep
 
     x_male = df['Male']
     x_female = df['Female']
     year = 2024
     population = x_male.sum() + x_female.sum()
+    dep = ((sum(x_male[0:7]) + sum(x_female[0:7]) + sum(x_male[32:]) + sum(x_female[32:])) 
+       / (sum(x_male[7:32]) + sum(x_female[7:32]))) * 100
 
     draw()
     fig.canvas.draw_idle()
+
+def mig(val):
+    global x_male
+    global x_female
+    global migration
+    global migamount
+    global split
+
+    boys = migamount * (split / 100)
+    girls = migamount * ((100 - split) / 100)
+
+    # Parameters of the normal distribution
+    mu = 0  # mean
+    sigma = 1  # standard deviation
+
+    nbins = 10
+    data = np.random.normal(mu, sigma, int(boys))
+    hist, bin_edges = np.histogram(data, bins=nbins)
+    for i in range(nbins):
+        x_male[migration//2 + i] += hist[i]    
+
+    data = np.random.normal(mu, sigma, int(girls))
+    hist, bin_edges = np.histogram(data, bins=nbins)
+    for i in range(nbins):
+        x_female[migration//2 + i] += hist[i]
+
+
+
 
  
 def draw():
@@ -95,12 +129,13 @@ def draw():
     global y
     global x_male
     global x_female
+    global dep
 
     ax1.clear()
     ax2.clear()
 
     text.remove()
-    text = plt.figtext(.7,.9,"Population: {:,}, Year: {}".format(int(population), year), ha='center')
+    text = plt.figtext(.7,.9,"Population: {:,}, Year: {}, Dependency: {}".format(int(population), year, int(dep)), ha='center')
 
     ax1.barh(y, x_female, align='center', color='lightpink')
     ax1.barh(y, x_male, align='center', color='blue')
@@ -109,36 +144,57 @@ def draw():
     ax2.barh(y, x_female, align='center', color='red')
     ax2.set(title='Females')
     ax1.invert_xaxis()
+
+    y_ticks = [0, 16, 33, 50, 66, 83, 100]
+    ax1.set_yticklabels(y_ticks)
+    ax2.set_yticklabels(y_ticks)
+
+
  
 
 # pause button
-axpause = plt.axes([0.075, 0.1, 0.1, 0.075])
+axpause = plt.axes([0.075, 0.05, 0.1, 0.075])
 bpause = Button(axpause, '{}'.format('run/pause'),color="grey")
 bpause.on_clicked(pause)
 
 # reset button
-axreset = plt.axes([0.19, 0.1, 0.1, 0.075])
+axreset = plt.axes([0.19, 0.05, 0.1, 0.075])
 breset = Button(axreset, 'reset',color="grey")
 breset.on_clicked(reset)
 
+# migrants button
+axmig = plt.axes([0.3, 0.05, 0.1, 0.075])
+bmig = Button(axmig, 'wave',color="grey")
+bmig.on_clicked(mig)
+
 # slider 1
-sax1 = plt.axes([0.075, .9, 0.3, 0.02])
-s1 = Slider(sax1, 'death rate', 10, 50, valinit=50)
+sax1 = plt.axes([0.075, .7, 0.3, 0.02])
+s1 = Slider(sax1, 'death rate', 10, 60, valinit=60)
 
 # slider 2
 fr = 1.83
-sax2 = plt.axes([0.075, .7, 0.3, 0.02])
-s2 = Slider(sax2, 'fertility rate', 0, 10, valinit=fr)
+sax2 = plt.axes([0.075, .6, 0.3, 0.02])
+s2 = Slider(sax2, 'fertility rate', 0, 5, valinit=fr)
 
-# slider 1
-split = 51
+# slider 3
+split = 50
 sax3 = plt.axes([0.075, .5, 0.3, 0.02])
 s3 = Slider(sax3, 'gender ratio', 0, 100, valinit=split)
 
-# slider 1
-migration = 0
-sax4 = plt.axes([0.075, .3, 0.3, 0.02])
-s4 = Slider(sax4, 'net migration', -30, 30, valinit=migration)
+# slider 4
+migration = 30
+sax4 = plt.axes([0.075, .4, 0.3, 0.02])
+s4 = Slider(sax4, 'average age', 20, 50, valinit=migration)
+
+# slider 5
+migamount = 0
+sax5 = plt.axes([0.075, .3, 0.3, 0.02])
+s5 = Slider(sax5, 'migration wave', 0, 500000, valinit=migamount)
+
+# slider 6
+amo = 0
+sax6 = plt.axes([0.075, .2, 0.3, 0.02])
+s6 = Slider(sax6, 'net migration', 0, 500000, valinit=amo)
 
 
 def update_s1(val):
@@ -167,10 +223,21 @@ def update_s4(val):
     migration = val
     fig.canvas.draw_idle()
 
+def update_s5(val):
+    global migamount
+    migamount = val
+    fig.canvas.draw_idle()
+
+def update_s6(val):
+    global amo
+    amo = val
+    fig.canvas.draw_idle()
+
+
 def logistic_function(x, x0):
     return 1 / (0.005 + np.exp(-0.1 * (x - x0)))
 
-x0 = 50
+x0 = 60
 probs = np.linspace(0, 100, 51)
 probs = logistic_function(probs, x0)
 probs /= 100
@@ -184,23 +251,42 @@ def update_plot(num):
     global fr
     global year
     global population
+    global migration
+    global dep
 
     if not started:
         return
     
     year += 1
     population = x_male.sum() + x_female.sum()
+    dep = ((sum(x_male[0:7]) + sum(x_female[0:7]) + sum(x_male[32:]) + sum(x_female[32:])) 
+       / (sum(x_male[7:32]) + sum(x_female[7:32]))) * 100
     
-    mothers = sum(x_female[7:25])
-    fathers = sum(x_male[7:25])
-    kids = (min(mothers, fathers) * fr) / 17
-    kids = kids + (migration * population / 1000)
-    boys = kids * (split / 100)
-    girls = kids * ((100 - split) / 100)
+    mothers = sum(x_female[7:20])
+    kids = (mothers * fr) / 12
+    
+    x_male = x_male.to_frame().mul(probs, axis = 0).shift(periods=1).fillna(kids/2, limit=1)['Male']
+    x_female = x_female.to_frame().mul(probs, axis = 0).shift(periods=1).fillna(kids/2, limit=1)['Female']
 
 
-    x_male = x_male.to_frame().mul(probs, axis = 0).shift(periods=1).fillna(boys, limit=1)['Male']
-    x_female = x_female.to_frame().mul(probs, axis = 0).shift(periods=1).fillna(girls, limit=1)['Female']
+    global amo
+    boys = amo * (split / 100)
+    girls = amo * ((100 - split) / 100)
+
+    # Parameters of the normal distribution
+    mu = 0  # mean
+    sigma = 1  # standard deviation
+
+    nbins = 10
+    data = np.random.normal(mu, sigma, int(boys))
+    hist, bin_edges = np.histogram(data, bins=nbins)
+    for i in range(nbins):
+        x_male[migration//2 + i] += hist[i]    
+
+    data = np.random.normal(mu, sigma, int(girls))
+    hist, bin_edges = np.histogram(data, bins=nbins)
+    for i in range(nbins):
+        x_female[migration//2 + i] += hist[i]
 
     age = 0
     wtf = 0
@@ -213,8 +299,6 @@ def update_plot(num):
         age += (i + 1) * value 
         wtf += value
     
-    print(age / wtf * 2)
-
     draw()
 
 
@@ -223,9 +307,18 @@ s1.on_changed(update_s1)
 s2.on_changed(update_s2)
 s3.on_changed(update_s3)
 s4.on_changed(update_s4)
+s5.on_changed(update_s5)
+s6.on_changed(update_s6)
 
 draw()
 
 ani = animation.FuncAnimation(fig, update_plot, interval=interval)
 
 plt.show()
+
+# youth/old ratio
+# slider for continuous amount of imigrants
+
+# one slider for migration age
+# one slider for migration gender
+# slider and button for instant amount of imigrants
